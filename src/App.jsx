6cd53772c50d280134b7648406832ab753c523cf
Maria Sievierova <129101728+Marys888@users.noreply.github.com>
2024-04-24 15:35:48 +0200

    changed netlify link

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <footer className='footer'>
         This App was created by <a href="https://github.com/Marys888">Mariia Sievierova</a> and 
         is open-sourced on <a href="https://github.com/Marys888/weather-react-app">GitHub</a> and 
-        hosted on <a href="https://weather-in-ur-city.netlify.app/">Netlify</a>
+        hosted on <a href="https://meteo-check.netlify.app/">Netlify</a>
       </footer>
       </div>
     </div>
